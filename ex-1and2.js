3e2c8add862c176f8cba74d47c593e463601e4f5
jerpuen <kittipot.kulpinit@gmail.com>
2024-08-19 10:48:08 +0700

    sent: assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lasrName = "Anderson";
+let address = "52/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const netPrice = 15000;
+const restaurantsOpen = 15000;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd#4Meeker, Colorado(CO), 81641";
+netPrice = 5000;
+restaurantsOpen = 2500;
+
+console.log(address);
+console.log(netPrice);
+console.log(restaurantsOpen);
